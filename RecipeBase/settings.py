86c14e8afc217d2fc841a48cbe76ff9585c1e3af
Your Name <djangoner6@gmail.com>
2022-07-15 18:58:42 +0300

    buid: static_root configured

--- RecipeBase/settings.py
+++ RecipeBase/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = "./static_all"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"

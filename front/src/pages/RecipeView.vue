e5fd37e1a2c7a92bddc96a542987780631e741b1
Your Name <djangoner6@gmail.com>
2022-07-29 11:52:40 +0300

    upd: recipe view fields

--- front/src/pages/RecipeView.vue
+++ front/src/pages/RecipeView.vue
@@ -194,89 +194,45 @@
               <!-- Recipe content -->
 
               <div class="q-pa-md q-gutter-sm" v-if="edit">
+                <q-input
+                  v-model="recipe.short_description"
+                  type="textarea"
+                  label="Короткое описание"
+                  hint="Показывается на карточке рецепта"
+                  :disable="!edit"
+                />
+
+                <h6 class="q-my-sm">Содержание (изначальное, источник)</h6>
+                <q-editor
+                  v-model="recipe.content_source"
+                  :dense="$q.screen.lt.md"
+                  :toolbar="editorToolbar"
+                  :fonts="editorFonts"
+                />
+                <h6 class="q-my-sm">Содержание (редактированное)</h6>
                 <q-editor
                   v-model="recipe.content"
                   :dense="$q.screen.lt.md"
-                  :toolbar="[
-                    [
-                      {
-                        label: $q.lang.editor.align,
-                        icon: $q.iconSet.editor.align,
-                        fixedLabel: true,
-                        list: 'only-icons',
-                        options: ['left', 'center', 'right', 'justify'],
-                      },
-                      {
-                        label: $q.lang.editor.align,
-                        icon: $q.iconSet.editor.align,
-                        fixedLabel: true,
-                        options: ['left', 'center', 'right', 'justify'],
-                      },
-                    ],
-                    ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
-                    ['token', 'hr', 'link', 'custom_btn'],
-                    ['print', 'fullscreen'],
-                    [
-                      {
-                        label: $q.lang.editor.formatting,
-                        icon: $q.iconSet.editor.formatting,
-                        list: 'no-icons',
-                        options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'code'],
-                      },
-                      {
-                        label: $q.lang.editor.fontSize,
-                        icon: $q.iconSet.editor.fontSize,
-                        fixedLabel: true,
-                        fixedIcon: true,
-                        list: 'no-icons',
-                        options: [
-                          'size-1',
-                          'size-2',
-                          'size-3',
-                          'size-4',
-                          'size-5',
-                          'size-6',
-                          'size-7',
-                        ],
-                      },
-                      {
-                        label: $q.lang.editor.defaultFont,
-                        icon: $q.iconSet.editor.font,
-                        fixedIcon: true,
-                        fixedIcon: true,
-                        list: 'no-icons',
-                        options: [
-                          'default_font',
-                          'arial',
-                          'arial_black',
-                          'comic_sans',
-                          'courier_new',
-                          'impact',
-                          'lucida_grande',
-                          'times_new_roman',
-                          'verdana',
-                        ],
-                      },
-                      'removeFormat',
-                    ],
-                    ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
-
-                    ['undo', 'redo'],
-                    ['viewsource'],
-                  ]"
-                  :fonts="{
-                    arial: 'Arial',
-                    arial_black: 'Arial Black',
-                    comic_sans: 'Comic Sans MS',
-                    courier_new: 'Courier New',
-                    impact: 'Impact',
-                    lucida_grande: 'Lucida Grande',
-                    times_new_roman: 'Times New Roman',
-                    verdana: 'Verdana',
-                  }"
+                  :toolbar="editorToolbar"
+                  :fonts="editorFonts"
                 />
               </div>
-              <div v-else class="recipe-text" v-html="recipe.content"></div>
+              <div v-else>
+                <div class="q-my-md" v-if="recipe.content_source">
+                  <span class="text-h6 q-my-sm text-primary"
+                    >Содержание (изначальное)</span
+                  >
+                  <div class="recipe-text" v-html="recipe.content_source"></div>
+                  <hr />
+                </div>
+
+                <div class="q-my-md">
+                  <span class="text-h6 q-my-sm text-primary">Содержание</span>
+
+                  <div class="recipe-text" v-html="recipe.content"></div>
+                  <hr />
+                </div>
+              </div>
 
               <div class="flex justify-around">
                 <q-btn
@@ -561,6 +517,77 @@ export default {
       is_main: false,
     };
 
+    let editorToolbar = [
+      [
+        {
+          label: this.$q.lang.editor.align,
+          icon: this.$q.iconSet.editor.align,
+          fixedLabel: true,
+          list: 'only-icons',
+          options: ['left', 'center', 'right', 'justify'],
+        },
+        {
+          label: this.$q.lang.editor.align,
+          icon: this.$q.iconSet.editor.align,
+          fixedLabel: true,
+          options: ['left', 'center', 'right', 'justify'],
+        },
+      ],
+      ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
+      ['token', 'hr', 'link', 'custom_btn'],
+      ['print', 'fullscreen'],
+      [
+        {
+          label: this.$q.lang.editor.formatting,
+          icon: this.$q.iconSet.editor.formatting,
+          list: 'no-icons',
+          options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'code'],
+        },
+        {
+          label: this.$q.lang.editor.fontSize,
+          icon: this.$q.iconSet.editor.fontSize,
+          fixedLabel: true,
+          fixedIcon: true,
+          list: 'no-icons',
+          options: ['size-1', 'size-2', 'size-3', 'size-4', 'size-5', 'size-6', 'size-7'],
+        },
+        {
+          label: this.$q.lang.editor.defaultFont,
+          icon: this.$q.iconSet.editor.font,
+          fixedIcon: true,
+          fixedIcon: true,
+          list: 'no-icons',
+          options: [
+            'default_font',
+            'arial',
+            'arial_black',
+            'comic_sans',
+            'courier_new',
+            'impact',
+            'lucida_grande',
+            'times_new_roman',
+            'verdana',
+          ],
+        },
+        'removeFormat',
+      ],
+      ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+
+      ['undo', 'redo'],
+      ['viewsource'],
+    ];
+
+    let editorFonts = {
+      arial: 'Arial',
+      arial_black: 'Arial Black',
+      comic_sans: 'Comic Sans MS',
+      courier_new: 'Courier New',
+      impact: 'Impact',
+      lucida_grande: 'Lucida Grande',
+      times_new_roman: 'Times New Roman',
+      verdana: 'Verdana',
+    };
+
     let requiredRule = (val) => !!val || 'Обязательное поле';
 
     return {
@@ -579,6 +606,8 @@ export default {
       ingList: null,
       requiredRule,
       ingredientsColumns,
+      editorToolbar,
+      editorFonts,
     };
   },
   mounted() {

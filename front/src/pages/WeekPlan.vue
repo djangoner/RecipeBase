828d92e086d8f6d8a05add8d6db8e047df35d6de
Your Name <djangoner6@gmail.com>
2022-07-30 08:28:21 +0300

    upd: week plan rewrited to WeekSelect comp

--- front/src/pages/WeekPlan.vue
+++ front/src/pages/WeekPlan.vue
@@ -1,19 +1,5 @@
 <template>
-  <q-bar>
-    <div class="col text-bold">
-      <div class="row justify-center items-center q-col-gutter-x-md">
-        <q-btn icon="chevron_left" flat @click="changeWeek(-1)"></q-btn>
-        <span class="cursor-pointer q-px-md"
-          >{{ year }}.{{ String(week).padStart(2, '0') }} ({{ getDay(0) }} -
-          {{ getDay(6) }})
-          <q-menu>
-            <q-date v-model="date_picker" first-day-of-week="1"></q-date>
-          </q-menu>
-        </span>
-        <q-btn icon="chevron_right" flat @click="changeWeek(+1)"></q-btn>
-      </div>
-    </div>
-  </q-bar>
+  <week-select v-model="week" @update:modeLValue="loadWeekPlan()" />
   <q-page padding>
     <div class="row wrap q-col-gutter-x-sm q-col-gutter-y-md">
       <!-- :class="$q.screen.lt.md ? 'column' : ''" -->
@@ -99,82 +85,41 @@
 </template>
 
 <script>
+import weekSelect, {
+  getDateOfISOWeek,
+  WeekDays,
+  getWeekNumber,
+} from 'components/WeekSelect.vue';
 import { useBaseStore } from 'src/stores/base';
 import { date } from 'quasar';
 
-function getWeekNumber(d) {
-  // Copy date so don't modify original
-  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-  // Set to nearest Thursday: current date + 4 - current day number
-  // Make Sunday's day number 7
-  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-  // Get first day of year
-  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  // Calculate full weeks to nearest Thursday
-  var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-  // Return array of year and week number
-  return [d.getUTCFullYear(), weekNo];
-}
-
-function getFirstDayOfWeek(d) {
-  const date = new Date(d);
-  const day = date.getDay();
-
-  const diff = date.getDate() - day + (day === 0 ? -6 : 1);
-
-  return diff;
-  // return new Date(date.setDate(diff));
-}
-
-function getDateOfISOWeek(y, w) {
-  var simple = new Date(y, 0, 1 + (w - 1) * 7);
-  var dow = simple.getDay();
-  var ISOweekStart = simple;
-  if (dow <= 4) ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
-  else ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
-  return ISOweekStart;
-}
-
-let WeekDays = {
-  1: 'Понедельник',
-  2: 'Вторник',
-  3: 'Среда',
-  4: 'Четверг',
-  5: 'Пятница',
-  6: 'Суббота',
-  7: 'Восскресенье',
-};
-
 export default {
+  components: { weekSelect },
   data() {
     const store = useBaseStore();
     return {
       store,
-      year: null,
-      week: null,
+      week: {
+        year: null,
+        week: null,
+      },
       loading: false,
-      date_picker: null,
       addMtimeSelect: null,
       search: '',
       WeekDays,
     };
   },
   mounted() {
-    this.selectToday();
-    // this.loadWeekPlan();
+    let [year, week] = getWeekNumber(new Date());
+    this.week.year = year;
+    this.week.week = week;
     this.loadMealTime();
   },
   methods: {
-    selectToday() {
-      let [year, week] = getWeekNumber(new Date());
-      this.year = year;
-      this.week = week;
-      this.changeWeek(0);
-    },
     loadWeekPlan() {
       let payload = {
-        year: this.year,
-        week: this.week,
+        year: this.week.year,
+        week: this.week.week,
       };
       this.loading = true;
 
@@ -252,49 +197,6 @@ export default {
           });
       });
     },
-    changeWeek(num) {
-      let min = 1;
-      let max = 53;
-
-      this.week += num;
-
-      if (this.week < min) {
-        this.week = max;
-        this.year -= 1;
-      } else if (this.week > max) {
-        this.week = min;
-        this.year += 1;
-      }
-      this.updateDatePicker();
-      this.loadWeekPlan();
-    },
-    updateDatePicker() {
-      // let d = new Date();
-      let d = getDateOfISOWeek(this.year, this.week);
-      let first = getFirstDayOfWeek(d);
-      let last = first + 6;
-      // console.debug(first, last);
-      let firstday = date.formatDate(
-        new Date(d.setDate(first)).toDateString(),
-        'YYYY/MM/DD'
-      );
-      let lastday = date.formatDate(
-        new Date(d.setDate(last)).toDateString(),
-        'YYYY/MM/DD'
-      );
-      this.date_picker = { from: firstday, to: lastday };
-      console.debug('Datepicker upd: ', this.date_picker, this.year, this.week);
-    },
-    parseDatePicker() {
-      if (typeof this.date_picker != 'string') {
-        return;
-      }
-      console.debug('Parsing date: ', this.date_picker);
-      let d = new Date(this.date_picker);
-      console.debug(d, getWeekNumber(d));
-      [this.year, this.week] = getWeekNumber(d);
-      this.changeWeek(0);
-    },
 
     filterRecipes(val, update, abort) {
       if (this.search == val && this.recipesList) {
@@ -364,9 +266,8 @@ export default {
       return raw.slice(0, raw.length - 3);
     },
     getDay(idx) {
-      let fday = new Date(this?.date_picker?.from);
-      let day = fday.getDay();
-      fday.setDate(fday.getDate() + day + idx - 1);
+      let fday = getDateOfISOWeek(this.week.year, this.week.week);
+      fday.setDate(fday.getDate() + idx - 1);
       return date.formatDate(fday, 'MM.DD');
     },
   },

ae07e1f77d02085099e6cd984014b852f9c04c24
Your Name <djangoner6@gmail.com>
2022-09-02 21:01:21 +0300

    fix: recipe saving

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -89,6 +89,7 @@ class RecipeSerializer(WritableNestedModelSerializer, serializers.ModelSerialize
     class Meta:
         model = Recipe
         exclude = ()
+        read_only_fields = ("author")
         depth = 2
 
     def get_short_description(self, obj: Recipe):

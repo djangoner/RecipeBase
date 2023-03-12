7752f904a122f30744e63093b8f13fb32c6a8451
Djangoner <djangoner6@gmail.com>
2023-03-12 16:53:07 +0200

    fix: pack count prefix

--- front/src/components/ProductListItems.vue
+++ front/src/components/ProductListItems.vue
@@ -40,7 +40,7 @@
             (
             <template v-if="item.packs && item.ingredient.item_weight">
               ~{{ Math.ceil(item.packs) }}
-              шт -
+              {{ item.ingredient.item_weight === 1000 ? "кг" : "шт" }}:
             </template>
             {{ item.amount }} {{ item.amount_type_str }}
             )

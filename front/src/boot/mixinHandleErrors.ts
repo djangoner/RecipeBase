fddcf8b03906056d0ca69f775b42ecfdac41f550
Djangoner <djangoner6@gmail.com>
2023-03-26 10:09:26 +0300

    upd: removed old handleErrors mixin

--- front/src/boot/mixinHandleErrors.ts
+++
@@ -1,9 +0,0 @@
-import { boot } from "quasar/wrappers";
-import HandleErrorsMixin from "src/modules/HandleErrorsMixin";
-
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(({ app } /* { app, router, ... } */) => {
-  // something to do
-  app.mixin(HandleErrorsMixin);
-});

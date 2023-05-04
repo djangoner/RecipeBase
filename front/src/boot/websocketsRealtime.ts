56d3421a5cde1e92526b6cf3eb1d70df007a7230
Djangoner <djangoner6@gmail.com>
2023-05-04 13:47:57 +0300

    upd: extracted websocket realtime to module

--- front/src/boot/websocketsRealtime.ts
+++ front/src/boot/websocketsRealtime.ts
@@ -1,6 +1,7 @@
 import { boot, store } from "quasar/wrappers"
 import { useBaseStore } from "src/stores/base"
 import ReconnectingWebSocket from "reconnecting-websocket"
+import { RealTime, StoreMappingObject, ModelUpdateData } from "src/modules/RealTime"
 
 const URL = (location.protocol == "https" ? "wss" : "ws") + "://" + location.host + "/ws/realtime"
 
@@ -12,102 +13,14 @@ const socket = new ReconnectingWebSocket(URL, [], {
 interface AnyData {
   [key: string]: unknown
 }
-
 interface StructureWebsocketData {
   type: string
   data: AnyData
 }
 
-interface ModelUpdateData extends StructureWebsocketData {
-  created: boolean
-  model: string
-}
-
-interface MappingGetSet {
-  get: CallableFunction
-  set: CallableFunction
-}
-
-type MappingValue = string | MappingGetSet
-
-interface Mapping {
-  idField?: string
-  single_attr?: MappingValue
-  array_attr?: MappingValue
-}
-
-interface MappingObject {
-  [key: string]: Mapping
-}
-
-function getStore(attr: MappingValue): unknown | unknown[] {
-  if (typeof attr === "string") {
-    // String
-    if (!Object.hasOwn(store, attr)) {
-      console.warn(`getStore hasn't attribute '${attr}'`)
-      return null
-    }
-    return store[attr]
-  } else {
-    // Getter
-    return attr.get()
-  }
-}
-
-function setStore(attr: MappingValue, value: unknown) {
-  if (typeof attr === "string") {
-    // String
-    if (!Object.hasOwn(store, attr)) {
-      console.warn(`setStore hasn't attribute '${attr}'`)
-      return null
-    }
-    store[attr] = value
-  } else {
-    // Setter
-    attr.set(value)
-  }
-}
-
-function onModelUpdate(data: ModelUpdateData) {
-  // console.debug("Processing model update...")
-  // -- Get model info
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const modelInfo = storeMapping[data.model]
-  if (!modelInfo) {
-    console.info(`Model info for ${data.model} not found, update was ignored.`)
-    return
-  }
-
-  const idField = modelInfo.idField || "id"
-  const newModel = data.data
-
-  // console.debug("Model info: ", modelInfo, getStore(modelInfo.single_attr))
-
-  if (modelInfo.single_attr) {
-    console.debug("Updated single attr")
-    setStore(modelInfo.single_attr, newModel)
-  }
-  if (modelInfo.array_attr) {
-    const arr_before_copy: unknown[] = getStore(modelInfo.array_attr)
-    const arr_before_idx = arr_before_copy?.findIndex((i) => i[idField] == newModel[idField])
-    if (arr_before_idx !== undefined) {
-      if (data.created) {
-        // Create
-        arr_before_copy.push(newModel)
-      } else {
-        // Update
-        arr_before_copy[arr_before_idx] = newModel
-      }
-      setStore(modelInfo.array_attr, arr_before_copy)
-      console.debug("Updated array attr")
-    }
-  }
-}
-
 const store = useBaseStore()
 
-const storeMapping: MappingObject = {
+const storeMapping: StoreMappingObject = {
   // const storeMapping = {
   ProductListItem: {
     single_attr: "product_list_item",
@@ -124,6 +37,8 @@ const storeMapping: MappingObject = {
     },
   },
 }
+
+const realTime = new RealTime(store, storeMapping)
 
 export default boot((/* { app, router, ... } */) => {
   console.debug("Socket init: ", socket)
@@ -142,7 +57,7 @@ export default boot((/* { app, router, ... } */) => {
     console.debug("[Socket] message: ", data)
 
     if (data.type == "model_update") {
-      onModelUpdate(data as ModelUpdateData)
+      realTime.onModelUpdate(data as ModelUpdateData)
     }
   }
 

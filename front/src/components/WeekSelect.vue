6721eb12321a7e698604e908b47953848344f902
Djangoner <djangoner6@gmail.com>
2023-07-24 15:21:07 +0300

    ref: refactored WeekSelect

--- front/src/components/WeekSelect.vue
+++ front/src/components/WeekSelect.vue
@@ -7,10 +7,7 @@
           flat
           @click="changeWeek(-1)"
         />
-        <span class="cursor-pointer q-px-md">{{ week_pick.year }}.{{
-          String(week_pick.week).padStart(2, "0")
-        }}
-          ({{ getDay(0) }} - {{ getDay(6) }})
+        <span class="cursor-pointer q-px-md">{{ week_pick.year }}.{{ String(week_pick.week).padStart(2, "0") }} ({{ getDay(0) }} - {{ getDay(6) }})
           <q-menu>
             <q-date
               v-model="date_picker"
@@ -28,140 +25,115 @@
   </q-bar>
 </template>
 
-<script lang="ts">
-import { useBaseStore } from "src/stores/base";
-import { date } from "quasar";
-import { defineComponent, PropType } from "vue";
-import {
-  DatePicker,
-  YearWeek,
-  YearWeekNullable,
-  WeekDays,
-  getDateOfISOWeek,
-  getFirstDayOfWeek,
-  getWeekNumber,
-  getYearWeek,
-} from "src/modules/WeekUtils";
+<script setup lang="ts">
+import { date } from "quasar"
+import { computed, onMounted, PropType, ref, Ref, watch } from "vue"
+import { DatePicker, YearWeek, YearWeekNullable, getDateOfISOWeek, getFirstDayOfWeek, getWeekNumber, getYearWeek } from "src/modules/WeekUtils"
 
-export default defineComponent({
-  props: {
-    modelValue: { required: true, type: Object as PropType<YearWeek> },
-  },
-  emits: ['update:model-value'],
-  data() {
-    const store = useBaseStore();
-    return {
-      store,
-      date_picker: null as DatePicker | string | null,
-      week_pick: {} as YearWeekNullable,
-      week_pick_old: {},
-      WeekDays,
-    };
-  },
-  watch: {
-    date_picker(val, oldVal) {
-      if (JSON.stringify(val) != JSON.stringify(oldVal)) {
-        this.parseDatePicker();
-      }
-    },
-    modelValue(val: YearWeek, oldVal: YearWeek) {
-      console.debug("modelValue: ", val, oldVal);
-      if (val !== oldVal) {
-        this.week_pick = val;
-      }
-    },
-    week_pick: {
-      deep: true,
-      handler: function (val: YearWeek) {
-        const notChanged =
-          JSON.stringify(val) === JSON.stringify(this.week_pick_old);
-        if (notChanged) {
-          this.week_pick_old = Object.assign({}, val);
-          return;
-        }
-        console.debug("week_pick: ", notChanged, val, this.week_pick_old);
-        this.week_pick_old = Object.assign({}, val);
-        this.$emit("update:model-value", val);
-      },
-    },
-  },
-  created() {
-    if (!this.modelValue.year || !this.modelValue.week) {
-      this.selectToday();
-    } else {
-      this.week_pick = this.modelValue;
-      this.updateDatePicker();
+const props = defineProps({
+  modelValue: { required: true, type: Object as PropType<YearWeek> },
+})
+const $emit = defineEmits(["update:model-value"])
+
+const date_picker: Ref<DatePicker | string | null> = ref(null)
+const week_pick: Ref<YearWeekNullable | Record<string, never>> = ref({})
+const week_pick_old: Ref<YearWeekNullable | Record<string, never>> = ref({})
+
+const modelValue = computed(() => props.modelValue)
+
+function selectToday() {
+  const [year, week] = getYearWeek()
+  week_pick.value.year = year
+  week_pick.value.week = week
+  changeWeek(0)
+}
+function changeWeek(num: number): void {
+  const min = 1
+  const max = 53
+  if (!week_pick.value.year || !week_pick.value.week) {
+    return
+  }
+
+  week_pick.value.week += num
+
+  if (week_pick.value.week < min) {
+    week_pick.value.week = max
+    week_pick.value.year -= 1
+  } else if (week_pick.value.week > max) {
+    week_pick.value.week = min
+    week_pick.value.year += 1
+  }
+  updateDatePicker()
+  // loadWeekPlan();
+}
+function updateDatePicker(): void {
+  if (!week_pick.value.year || !week_pick.value.week) {
+    return
+  }
+  // let d = new Date();
+  const d = getDateOfISOWeek(week_pick.value.year, week_pick.value.week)
+  const first = getFirstDayOfWeek(d)
+  const last = first + 6
+  // console.debug(first, last);
+  const firstday = date.formatDate(new Date(d.setDate(first)).toDateString(), "YYYY/MM/DD")
+  const lastday = date.formatDate(new Date(d.setDate(last)).toDateString(), "YYYY/MM/DD")
+  date_picker.value = { from: firstday, to: lastday }
+  console.debug("Datepicker upd: ", date_picker, week_pick.value.year)
+}
+function parseDatePicker(): void {
+  if (typeof date_picker.value != "string") {
+    return
+  }
+  console.debug("Parsing date: ", date_picker)
+  const d = new Date(date_picker.value)
+  console.debug(d, getWeekNumber(d))
+  ;[week_pick.value.year, week_pick.value.week] = getWeekNumber(d)
+  changeWeek(0)
+}
+function getDay(idx: number): string | undefined {
+  if (!date_picker.value || typeof date_picker.value == "string") {
+    return
+  }
+  const fday = new Date(date_picker.value.from)
+  // let fday = getDateOfISOWeek(week_pick.year, week_pick.week);
+  const day = fday.getDay()
+  fday.setDate(fday.getDate() + day + idx - 1)
+  return date.formatDate(fday, "DD.MM")
+}
+
+onMounted(() => {
+  if (!modelValue.value.year || !modelValue.value.week) {
+    selectToday()
+  } else {
+    week_pick.value = modelValue.value
+    updateDatePicker()
+  }
+})
+
+watch(date_picker, (val, oldVal) => {
+  if (JSON.stringify(val) != JSON.stringify(oldVal)) {
+    parseDatePicker()
+  }
+})
+watch(modelValue, (val: YearWeek, oldVal: YearWeek) => {
+  console.debug("modelValue: ", val, oldVal)
+  if (val !== oldVal) {
+    week_pick.value = val
+  }
+})
+
+watch(
+  week_pick,
+  (val: YearWeek) => {
+    const notChanged = JSON.stringify(val) === JSON.stringify(week_pick_old)
+    if (notChanged) {
+      week_pick_old.value = Object.assign({}, val)
+      return
     }
+    console.debug("week_pick: ", notChanged, val, week_pick_old)
+    week_pick_old.value = Object.assign({}, val)
+    $emit("update:model-value", val)
   },
-  methods: {
-    selectToday() {
-      const [year, week] = getYearWeek();
-      this.week_pick.year = year;
-      this.week_pick.week = week;
-      this.changeWeek(0);
-    },
-    changeWeek(num: number): void {
-      const min = 1;
-      const max = 53;
-      if (!this.week_pick.year || !this.week_pick.week) {
-        return;
-      }
-
-      this.week_pick.week += num;
-
-      if (this.week_pick.week < min) {
-        this.week_pick.week = max;
-        this.week_pick.year -= 1;
-      } else if (this.week_pick.week > max) {
-        this.week_pick.week = min;
-        this.week_pick.year += 1;
-      }
-      this.updateDatePicker();
-      // this.loadWeekPlan();
-    },
-    updateDatePicker(): void {
-      if (!this.week_pick.year || !this.week_pick.week) {
-        return;
-      }
-      // let d = new Date();
-      const d = getDateOfISOWeek(this.week_pick.year, this.week_pick.week);
-      const first = getFirstDayOfWeek(d);
-      const last = first + 6;
-      // console.debug(first, last);
-      const firstday = date.formatDate(
-        new Date(d.setDate(first)).toDateString(),
-        "YYYY/MM/DD"
-      );
-      const lastday = date.formatDate(
-        new Date(d.setDate(last)).toDateString(),
-        "YYYY/MM/DD"
-      );
-      this.date_picker = { from: firstday, to: lastday };
-      console.debug("Datepicker upd: ", this.date_picker, this.week_pick.year);
-    },
-    parseDatePicker(): void {
-      if (typeof this.date_picker != "string") {
-        return;
-      }
-      console.debug("Parsing date: ", this.date_picker);
-      const d = new Date(this.date_picker);
-      console.debug(d, getWeekNumber(d));
-      [this.week_pick.year, this.week_pick.week] = getWeekNumber(d);
-      this.changeWeek(0);
-    },
-    // timeFormat(raw): string {
-    //   return raw.slice(0, raw.length - 3);
-    // },
-    getDay(idx: number): string | undefined {
-      if (!this.date_picker || typeof this.date_picker == "string") {
-        return;
-      }
-      const fday = new Date(this.date_picker.from);
-      // let fday = getDateOfISOWeek(this.week_pick.year, this.week_pick.week);
-      const day = fday.getDay();
-      fday.setDate(fday.getDate() + day + idx - 1);
-      return date.formatDate(fday, "DD.MM");
-    },
-  },
-});
+  { deep: true }
+)
 </script>

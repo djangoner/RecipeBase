cfea850f8a97fe618fe24f50757601bf8d5eb745
Djangoner <djangoner6@gmail.com>
2023-08-13 16:03:27 +0300

    chore: fix gitignore

---
+++ front/src/stores/cache.ts
@@ -0,0 +1,84 @@
+import { defineStore } from "pinia"
+import { SessionStorage } from "quasar"
+import { stringify, parse } from "zipson"
+
+type CacheKey = string | object
+type CacheValue = object
+
+interface CacheMeta {
+  value: CacheValue
+  valid_until: number // Timestamp
+  // ttl: number
+}
+type CacheData = Map<string, CacheMeta>
+
+function readCache(): CacheData {
+  const rawString: string = SessionStorage.getItem("CacheStorage") || ""
+  const data = new Map(Object.entries((parse(rawString) as object) || {})) as CacheData
+  console.debug("Read cache: ", rawString.length)
+  return data
+}
+
+export const useCacheStore = defineStore("cache", {
+  state() {
+    return {
+      cacheData: readCache(),
+      lastCleared: 0,
+    }
+  },
+  getters: {
+    getCached: (state) => (key: CacheKey) => {
+      const meta = state.cacheData.get(JSON.stringify(key))
+      if (!meta) {
+        return
+      } else if (meta.valid_until < Date.now()) {
+        state.cacheData.delete(JSON.stringify(key))
+        return
+      }
+      return meta.value
+    },
+  },
+
+  actions: {
+    writeCache() {
+      // const serialized = JSON.stringify(Array.from(this.cacheData.entries()))
+      const serialized = Object.fromEntries(this.cacheData.entries())
+      SessionStorage.set("CacheStorage", stringify(serialized))
+    },
+    readCache() {
+      return readCache()
+    },
+    setCached(key: CacheKey, value: CacheValue, ttl = 60 * 1000) {
+      const meta: CacheMeta = {
+        value: value,
+        valid_until: Date.now() + ttl,
+      }
+      this.cacheData.set(JSON.stringify(key), meta)
+      this.writeCache()
+
+      if (Date.now() - this.lastCleared > 30*1000){
+        this.clearOutdated()
+        this.lastCleared = Date.now()
+      }
+    },
+    delCached(key: CacheKey) {
+      this.cacheData.delete(JSON.stringify(key))
+      this.writeCache()
+    },
+    clearOutdated() {
+      const now = Date.now()
+      console.debug("[Cache] clearing outdated cache...")
+      const sizeBefore = this.cacheData.size
+      this.cacheData.forEach((value: CacheMeta, key: string) => {
+        if (value.valid_until < now) {
+          this.cacheData.delete(key)
+        }
+      })
+      const sizeAfter = this.cacheData.size
+      console.debug(`[Cache] cleared: (${sizeBefore} -> ${sizeAfter})`)
+      if (sizeAfter != sizeBefore){
+        this.writeCache()
+      }
+    },
+  },
+})
